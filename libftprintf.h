a4e1134ded969cfc609db44b2e96feff2f1b0c04
b-in-R <raphael.biner@gmail.com>
2024-12-08 16:47:56 +0100

    header + .h

---
+++ libftprintf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rabiner <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 16:43:22 by rabiner           #+#    #+#             */
+/*   Updated: 2024/12/08 16:43:24 by rabiner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+
+/*
+struct si besoin
+*/
+
+int	ft_printf(const char *format, ...);
+
+#endif
